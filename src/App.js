beef9fd7130b8d49e9ef04fc13b82e2ec0aabe9b
suber-IQ <suberquercusumit@gmail.com>
2022-12-03 20:33:51 +0530

    First day some work frontend

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import Courses from './components/Courses/Courses'
+import Home from './components/Home/Home'
+import Header from './components/Layout/Header/Header'
+
+const App = () => {
+  return (
+    <Router>
+       <Header />
+       <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/courses' element={<Courses />} />
+       </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/components/Courses/Courses.jsx
@@ -0,0 +1,122 @@
+import React, { useState } from 'react';
+import {
+  Button,
+  Container,
+  Heading,
+  HStack,
+  Image,
+  Input,
+  Link,
+  Stack,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import styles from "./Courses.module.css"
+
+
+const Course = ({ views, title, imageSrc, id, addToPlaylistHandler, creator, description, lectureCount}) => {
+    return (
+           <VStack className={styles.course} alignItems={["center", "flex-start"]}>
+              <Image src={imageSrc} boxSize="60" objectFit={"contain"} />
+              <Heading textAlign={["center", "left"]} maxW="200px" fontFamily={"sans-serif"} noOfLines={3} children={title} size="sm" />
+              <Text noOfLines={2} children={description} />
+              <HStack>
+              <Text fontWeight={"bold"} textTransform={"uppercase"} children={"creator"} />
+              <Text fontFamily={"body"} textTransform={"uppercase"} children={creator} />
+              </HStack>
+              <Heading textAlign={"center"} size="xs" children={`Lectures - ${lectureCount}`}
+              textTransform={"uppercase"}
+               />
+              <Heading size="xs" children={`Views - ${views}`}
+              textTransform={"uppercase"}
+               />
+               <Stack direction={["column", "row"]} alignItems="center">
+                  <Link  to={`/course/${id}`}>
+                    <Button colorScheme={"yellow"}>
+                        Watch Now
+                    </Button>
+                  </Link>
+                    <Button variant={"ghost"} colorScheme={"yellow"} onClick={() => addToPlaylistHandler(id)}>
+                        Add to playlist
+                    </Button>
+
+               </Stack>
+           </VStack>
+    )
+}
+
+
+const Courses = () => {
+  const [keyword, setKeyword] = useState('');
+  const [category, setCategory] = useState('');
+
+
+  const addToPlaylistHandler = () => {
+    console.log("Added to Playlist")
+  }
+
+  const categories = [
+    'Web Development',
+    'Artificial Intellegence',
+    'Data Structure & Algorithm',
+    'App Development',
+    'Data Science',
+    'Game Development',
+  ];
+
+  return (
+    <Container minH={'95vh'} maxW="container.lg" paddingY={'8'}>
+      <Heading children="All Courses" m={'8'} />
+      <Input
+        type="text"
+        value={keyword}
+        onChange={e => setKeyword(e.target.value)}
+        placeholder="Search a course..."
+        focusBorderColor="yellow.500"
+      />
+      <HStack
+        overflowX="auto"
+        paddingY={'8'}
+        css={{
+          '&::-webkit-scrollbar': {
+            width: '1px',
+          },
+          '&::-webkit-scrollbar-track': {
+            width: '2px',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            background: 'aqua',
+            borderRadius: '4px',
+          },
+        }}
+      >
+        {categories.map((item, index) => {
+          return (
+            <Button key={index} onClick={() => setCategory(item)} minW={'60'}>
+              <Text children={item} />
+            </Button>
+          );
+        })}
+      </HStack>
+      <Stack
+       direction={["column", "row"]}
+       flexWrap="wrap"
+       justifyContent={["flex-start", "space-evenly"]}
+       alignItems={["center", "flex-start"]}
+      >
+       <Course
+        title={"Sample"}
+        description={"Sample lite"} 
+        views={23}
+        imageSrc={"Sample"}
+        id={"Sample id"}
+        creator={"Sumit Kumar"}
+        lectureCount={2}
+        addToPlaylistHandler={addToPlaylistHandler}
+        />
+      </Stack>
+    </Container>
+  );
+};
+
+export default Courses;
