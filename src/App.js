807580ef2cd9b8f6c0cfb35393ab06c9392b8a4c
suber-IQ <suberquercusumit@gmail.com>
2022-12-08 22:27:41 +0530

    Making Dashboard in component file makee

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import About from './components/About/About'
+import AdminCourses from './components/Admin/AdminCourses/AdminCourses'
+import CreateCourse from './components/Admin/CreateCourse/CreateCourse'
+import Dashboard from './components/Admin/Dashboard/Dashboard'
+import Users from './components/Admin/Users/Users'
 import ForgetPassword from './components/Auth/ForgetPassword'
 import Login from './components/Auth/Login'
 import Register from './components/Auth/Register'
@@ -15,7 +19,9 @@ import NotFound from './components/Layout/NotFound/NotFound'
 import PaymentFail from './components/Payments/PaymentFail'
 import PaymentSuccess from './components/Payments/PaymentSuccess'
 import Subscribe from './components/Payments/Subscribe'
+import ChangePassword from './components/Profile/ChangePassword'
 import Profile from './components/Profile/Profile'
+import UpdateProfile from './components/Profile/UpdateProfile'
 import Request from './components/Request/Request'
 
 const App = () => {
@@ -30,8 +36,18 @@ const App = () => {
           <Route path='/courses' element={<Courses />} />
           <Route path='/course/:id' element={<CoursePage />} />
 
-          {/* Authencation and Info */}
+           {/* Admin Routes */}
+          <Route path='/admin/dashboard' element={<Dashboard />} />
+          <Route path='/admin/createcourse' element={<CreateCourse />} />
+          <Route path='/admin/courses' element={<AdminCourses />} />
+          <Route path='/admin/users' element={<Users />} />
+
+          {/* Authencation with profile and info */}
           <Route path='/profile' element={<Profile />} />
+          <Route path='/changepassword' element={<ChangePassword />} />
+          <Route path='/updateprofile' element={<UpdateProfile />} />
+
+           {/* Authencation  */}
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/contact' element={<Contact />} />
