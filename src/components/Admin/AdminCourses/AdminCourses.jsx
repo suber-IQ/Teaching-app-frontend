97982da6a123e6cb88def9369bc0c5d36ce846be
suber-IQ <suberquercusumit@gmail.com>
2022-12-10 10:32:46 +0530

    frontend work complete with Admin file

--- src/components/Admin/AdminCourses/AdminCourses.jsx
+++ src/components/Admin/AdminCourses/AdminCourses.jsx
@@ -1,22 +1,144 @@
-import { Box, Grid } from '@chakra-ui/react'
-import React from 'react'
-import Sidebar from '../Sidebar'
+import {
+  Box,
+  Button,
+  Grid,
+  Heading,
+  HStack,
+  Image,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  useDisclosure,
+} from '@chakra-ui/react';
+import React from 'react';
+import Sidebar from '../Sidebar';
 import cursor from '../../../assets/images/cursor.png';
-
+import { RiDeleteBin7Fill } from 'react-icons/ri';
+import CourseModal from './CourseModal';
 
 const AdminCourses = () => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
+  const courses = [
+    {
+      _id: 'jllasfjla',
+      title: 'Rectjs',
+      category: 'web development',
+      poster: {
+        url: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.edQKztD6G9khB_APGqkhOwHaEK%26pid%3DApi&f=1&ipt=a2f6e728a6783c2416be6841a8e1d10aa09bce9a64bdc4ce43cfc83dfd1f825c&ipo=images',
+      },
+      createdBy: 'suber-Iq',
+      views: 123,
+      numOfVideos: 23,
+    },
+  ];
+
+  const courseDetailsHandler = userId => {
+    onOpen();
+  };
+  const deleteButtonHandler = userId => {
+    console.log(userId);
+  };
+  const deleteLectureButtonHandler = (courseId, lectureId) => {
+    console.log(courseId);
+    console.log(lectureId);
+  };
+  const addLectureHandler = (e,courseId, title, description, video) => {
+    e.preventDefault();
+
+  }
+
   return (
     <Grid
-    css={{
-      cursor: `url(${cursor}), default`,
-    }}
-    minH={'100vh'}
-    templateColumns={['1fr', '5fr 1fr']}
-  >
-    <Box></Box>
-    <Sidebar />
-  </Grid>
-  )
+      css={{
+        cursor: `url(${cursor}), default`,
+      }}
+      minH={'100vh'}
+      templateColumns={['1fr', '5fr 1fr']}
+    >
+      <Box p={['0', '8']} overflowX="auto">
+        <Heading
+          textTransform={'uppercase'}
+          children="All Users"
+          my={'16'}
+          textAlign={['center', 'left']}
+        />
+        <TableContainer w={['100vw', 'full']}>
+          <Table variant={'simple'} size="lg">
+            <TableCaption>All available courses in the database</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>Id</Th>
+                <Th>Poster</Th>
+                <Th>Title</Th>
+                <Th>Creator</Th>
+                <Th isNumeric>Views</Th>
+                <Th isNumeric>Lectures</Th>
+                <Th isNumeric>Action</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {courses.map(item => (
+                <Row
+                  courseDetailsHandler={courseDetailsHandler}
+                  deleteButtonHandler={deleteButtonHandler}
+                  key={item._id}
+                  item={item}
+                />
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+        <CourseModal
+          isOpen={isOpen}
+          onClose={onClose}
+          id={"alsjflasjfl"}
+          courseTitle="React Course"
+          deleteButtonHandler={deleteLectureButtonHandler}
+          addLectureHandler={addLectureHandler}
+        />
+      </Box>
+      <Sidebar />
+    </Grid>
+  );
+};
+
+function Row({ item, courseDetailsHandler, deleteButtonHandler }) {
+  return (
+    <Tr>
+      <Td>{item._id}</Td>
+      <Td>
+        <Image src={item.poster.url} />
+      </Td>
+      <Td>{item.title}</Td>
+      <Td textTransform={'uppercase'}>{item.category}</Td>
+      <Td>{item.createdBy}</Td>
+      <Td isNumeric>{item.views}</Td>
+      <Td isNumeric>{item.numOfVideos}</Td>
+      <Td isNumeric>
+        <HStack justifyContent={'flex-end'}>
+          <Button
+            onClick={() => courseDetailsHandler(item._id)}
+            variant={'outline'}
+            color="purple.500"
+          >
+            View Lectures
+          </Button>
+          <Button
+            onClick={() => deleteButtonHandler(item._id)}
+            color={'purple.600'}
+          >
+            <RiDeleteBin7Fill />
+          </Button>
+        </HStack>
+      </Td>
+    </Tr>
+  );
 }
 
-export default AdminCourses
+export default AdminCourses;
